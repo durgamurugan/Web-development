45cc62504e5e96bac9335092bb80018728ed6e3e
Durgadevi Murugan Tevar <34877238+durgamurugan@users.noreply.github.com>
2021-05-21 20:10:20 +0530

    Add files via upload
    3d caurosel

---
+++ 3d Carousel/Caurosel.js
@@ -0,0 +1,22 @@
+var cubex = 0,    // initial rotation
+cubey = 0,
+cubez = 0;
+function rotate(variableName, degrees) {
+    window[variableName] = window[variableName] + degrees;
+    rotCube(cubex, cubey, cubez);
+}
+function rotCube(degx, degy, degz){
+    segs = "rotateX("+degx+"deg) rotateY("+degy+"deg) rotateZ("+degz+"deg) translateX(0) translateY(0) translateZ(0)";
+    $('#D3Cube').css({"transform":segs});
+}
+
+function flipCube() {
+    rotate("cubex", 90);
+}
+
+// function turnRight() {
+//     rotate("cubey", 90);
+// }
+// function turnLeft() {
+//     rotate("cubey", -90);
+// }
